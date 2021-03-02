3009c2cec30d0f9da0dd8751a640fc49b0e9c51b
TravisSLee <travisslee223@gmail.com>
2021-03-02 17:13:31 -0500

    commit tree fd4f7f2947a870a11631c9b4a250adb4edae0d01

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+function saturdayFun(fun = 'roller-skate'){
+    return `This Saturday, I want to ${fun}!`
+}
+
+function mondayWork( work = 'go to the office') {
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective( string = '*'){
+    return function( smthg = 'special'){
+        return `You are ${string}${smthg}${string}!`
+    }
+}
+
+const Calculator = {}
+
+Calculator.add = function(){
+    return 1 + 3; 
+}
+Calculator.subtract = function(){
+    return 1 - 3;
+}
+Calculator.multiply = function(){
+    return 1 * 3;
+}
+Calculator.divide = function(){
+    return 10 / 5;
+};
+
+function actionApplyer(num, arr){
+    for (let i = 0; i < arr.length; i++) {
+        num = arr[i](num);
+      }
+      return num;
+}
